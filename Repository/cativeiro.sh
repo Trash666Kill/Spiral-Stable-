e2e8d8ad55033636d1494e9a51b9b6df4668a013
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2022-11-21 19:05:54 -0300

    Update cativeiro.sh

--- Repository/cativeiro.sh
+++ Repository/cativeiro.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #
-autossh -M 0 -N -R 2222:localhost:22 -p 4632 emperor@cativeiro.hopto.org
+autossh -M 0 -N -R 2222:localhost:22 -p 4632 emperor@cativeiro.hopto.org -o StrictHostKeyChecking=false
 #
