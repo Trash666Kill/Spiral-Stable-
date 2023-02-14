9c19e0c3b92ff7723d73aa74b8c7cd804f1d80f7
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2023-02-14 06:52:59 -0300

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -11,7 +11,7 @@ apt update && apt upgrade -y
 #Base packages*
 echo "**INSTALLING BASE PACKAGES**"
 echo "1"
-apt install sudo cryptsetup smartmontools vim sshfs systemd-timesyncd xz-utils uuid pigz -y
+apt install sudo cryptsetup smartmontools vim sshfs systemd-timesyncd xz-utils uuid pigz sshpass -y
 echo "2"
 apt install lm-sensors htop iotop stress hdparm x11-xkb-utils bc fwupd -y
 echo "3"
