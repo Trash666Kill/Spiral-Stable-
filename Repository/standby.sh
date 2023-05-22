266fbb80cd73e290f17ad9c107877155ff8494c9
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2023-05-22 13:47:05 -0300

    Update standby.sh

--- Repository/standby.sh
+++ Repository/standby.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 #
-echo "##STANDBY MODE STARTED##"
+#echo "##STANDBY MODE STARTED##"
 #
-echo "##Shutting down virtual machines##"
+#echo "##Shutting down virtual machines##"
 for i in $(virsh list | grep running | awk '{print $2}'); do virsh shutdown $i; done
 sleep 60
 virsh list --all
 #
-echo "##Stopping services##"
-echo "**Samba**"
+#echo "##Stopping services##"
+#echo "**Samba**"
 systemctl stop smbd
-echo "**Zabbix**"
+#echo "**Zabbix**"
 systemctl stop zabbix-agent
-echo "**libvirt**"
+#echo "**libvirt**"
 systemctl stop libvirtd
 sleep 04
 #
-echo "##Putting Storage Drives in Standby Mode##"
-echo "**Dismantling**"
+#echo "##Putting Storage Drives in Standby Mode##"
+#echo "**Dismantling**"
 umount /mnt/Local/Container-A
 umount /mnt/Local/Container-B
 umount /mnt/Local/Container-C
-echo "**Closing LUKS units**"
+#echo "**Closing LUKS units**"
 cryptsetup close Container-A_crypt
 cryptsetup close Container-B_crypt
 cryptsetup close Container-C_crypt
-echo "**Ejecting units**"
+#echo "**Ejecting units**"
 eject /dev/sdb
 eject /dev/sdc
