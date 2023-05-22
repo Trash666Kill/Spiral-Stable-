433d133561c584e62876fc80e4b8ed54663abbc1
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2023-05-22 13:08:15 -0300

    Add files via upload

---
+++ Repository/standby.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+(
+echo "##STANDBY MODE STARTED##"
+#
+echo "##Shutting down virtual machines##"
+for i in $(virsh list | grep running | awk '{print $2}'); do virsh shutdown $i; done
+sleep 60
+virsh list --all
+#
+echo "##Stopping services##"
+echo "**Samba**"
+systemctl stop smbd
+echo "**Zabbix**"
+systemctl stop zabbix-agent
+echo "**libvirt**"
+systemctl stop libvirtd
+sleep 04
+#
+echo "##Putting Storage Drives in Standby Mode##"
+echo "**Dismantling**"
+umount /mnt/Local/Container-B
+umount /mnt/Local/Container-C
+echo "**Closing LUKS units**"
+cryptsetup close Container-B_crypt
+cryptsetup close Container-C_crypt
+echo "**Ejecting units**"
+eject /dev/sdb
+eject /dev/sdc
+) 2>&1 | tee standby-`date +%F`.log
