1e60875363e0bf880a4e42eb032bd69903af0a9b
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2023-05-22 13:34:55 -0300

    Update mount.sh

--- Repository/mount.sh
+++ Repository/mount.sh
@@ -35,4 +35,4 @@ df -hT /dev/mapper/Container-A_crypt
 df -hT /dev/mapper/Container-B_crypt
 df -hT /dev/mapper/Container-C_crypt
 #df -hT /mnt/Remote/Servers/SRV02/Container-A/Virt/Images
-) 2>&1 | tee mount-`date +%F`.log
+) >> mount-`date +%F`.log
