7dbcb6d66aa244a7567d4fed3f643211af1d9ba5
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2023-05-26 15:46:16 -0300

    Update avscan.sh

--- Repository/avscan.sh
+++ Repository/avscan.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 systemctl stop clamav-freshclam
 freshclam --quiet
-clamscan --recursive --infected --log=avscan-`date +%F`.log --move=/root/.isolation /mnt/Remote/Servers/SRV01/Container-C
+clamscan --recursive --infected --log=/var/log/clamav/avscan-`date +%F`.log --move=/root/.isolation /mnt/Remote/Servers/SRV01/Container-C
 systemctl restart clamav-freshclam
