853607fe3f96e7ec8b1f64e3aecc996e8a914d8e
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2023-05-26 14:33:14 -0300

    Update avscan.sh

--- Repository/avscan.sh
+++ Repository/avscan.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-clamscan --recursive --infected --log= --exclude-dir='^/Virt|^/Temp/ISO' --move=/root/.isolation /mnt/Remote/Servers/SRV01/Container-C
+clamscan --recursive --infected --log=-result-`date +%F`.log --exclude-dir='^/Virt|^/Temp/ISO' --move=/root/.isolation /mnt/Remote/Servers/SRV01/Container-C
