89f8720e963dd986822766b077ee103f9b5c4233
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2023-05-27 20:20:48 -0300

    Update avscan.sh

--- Repository/avscan.sh
+++ Repository/avscan.sh
@@ -3,10 +3,10 @@
 set -e
 sshfs -p 26 emperor@SRV01:/mnt/Local/Container-C/ /mnt/Remote/Servers/SRV01/Container-C/ -o allow_other -o compression=no -o StrictHostKeyChecking=false
 #
-systemctl stop clamav-freshclam
-freshclam --quiet
+#systemctl stop clamav-freshclam
+#freshclam --quiet
 clamscan --recursive --infected --log=/var/log/clamav/daily/avscan-`date +%F`.log --move=/root/.isolation /mnt/Remote/Servers/SRV01/Container-C
-systemctl restart clamav-freshclam
+#systemctl restart clamav-freshclam
 #
 umount /mnt/Remote/Servers/SRV01/Container-C/
 #
